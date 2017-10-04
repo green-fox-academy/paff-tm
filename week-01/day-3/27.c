c3494c9fc53f4e329f5fefdf978fa01c83bfef70
paff-tm <4ndras@gmail.com>
2017-10-04 16:21:42 +0200

    updates 27.c

--- week-01/day-3/27.c
+++ week-01/day-3/27.c
@@ -7,18 +7,43 @@ print out first the original values then print out the swaped values
 but this time use bitwise operators only
 */
 
+#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
+#define BYTE_TO_BINARY(byte)  \
+  (byte & 0x80 ? '1' : '0'), \
+  (byte & 0x40 ? '1' : '0'), \
+  (byte & 0x20 ? '1' : '0'), \
+  (byte & 0x10 ? '1' : '0'), \
+  (byte & 0x08 ? '1' : '0'), \
+  (byte & 0x04 ? '1' : '0'), \
+  (byte & 0x02 ? '1' : '0'), \
+  (byte & 0x01 ? '1' : '0')
+
+
 int main()
 {
     int x = 32;
     int y = 20;
 
-    printf("original:\nx = %d\ny = %d\n\n", x, y);
+    printf("original:\n", x, y);
+    printf("x = %d = "BYTE_TO_BINARY_PATTERN"\n", x, BYTE_TO_BINARY(x));
+    printf("y = %d = "BYTE_TO_BINARY_PATTERN"\n\n", y,  BYTE_TO_BINARY(y));
 
+    printf("x = x ^ y\n");
     x = x ^ y;
+    printf("x = %d = "BYTE_TO_BINARY_PATTERN"\n", x, BYTE_TO_BINARY(x));
+    printf("y = %d = "BYTE_TO_BINARY_PATTERN"\n\n", y,  BYTE_TO_BINARY(y));
+
+    printf("y = x ^ y\n");
     y = x ^ y;
-    x = x ^ y;
+    printf("x = %d = "BYTE_TO_BINARY_PATTERN"\n", x, BYTE_TO_BINARY(x));
+    printf("y = %d = "BYTE_TO_BINARY_PATTERN"\n\n", y,  BYTE_TO_BINARY(y));
 
-    printf("swapped:\nx = %d\ny = %d\n", x, y);
+    printf("x = x ^ y\n");
+    x = x ^ y;
+    printf("x = %d = "BYTE_TO_BINARY_PATTERN"\n", x, BYTE_TO_BINARY(x));
+    printf("y = %d = "BYTE_TO_BINARY_PATTERN"\n\n", y,  BYTE_TO_BINARY(y));
+
+    printf("Tadaaa...!\n");
 
     return(0);
 
