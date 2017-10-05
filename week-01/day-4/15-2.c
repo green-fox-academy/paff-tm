8deb619840bcd628b95d9f001191bef08f6d9123
paff-tm <4ndras@gmail.com>
2017-10-05 16:27:56 +0200

    replace amounts to variables

--- week-01/day-4/15-2.c
+++ week-01/day-4/15-2.c
@@ -23,73 +23,74 @@ Take care of the rounding (when giving back 5 coins):
 
 int main()
 {
-    int amount_of_money = 57552;
-    int m;
+    int amount_of_money = 57553;
+    int c1 = 20000, c2 = 10000, c3 = 5000, c4 = 2000, c5 = 1000, c6 = 500, c7 = 200, c8= 100, c9 = 50, c10 = 20, c11 = 10, c12 = 5;
+    int m; // remaining money
 
-    m = amount_of_money / 20000;
-    amount_of_money -= m * 20000;
+    m = amount_of_money / c1;
+    amount_of_money -= m * c1;
     if (m > 0)
-        printf("%d-20000\n", m);
+        printf("%d-%d\n", m, c1);
 
-    m = amount_of_money / 10000;
-    amount_of_money -= m * 10000;
+    m = amount_of_money / c2;
+    amount_of_money -= m * c2;
     if (m > 0)
-        printf("%d-10000\n", m);
+        printf("%d-%d\n", m, c2);
 
-    m = amount_of_money / 5000;
-    amount_of_money -= m * 5000;
+    m = amount_of_money / c3;
+    amount_of_money -= m * c3;
     if (m > 0)
-        printf("%d-5000\n", m);
+        printf("%d-%d\n", m, c3);
 
-    m = amount_of_money / 2000;
-    amount_of_money -= m * 2000;
+    m = amount_of_money / c4;
+    amount_of_money -= m * c4;
     if (m > 0)
-        printf("%d-2000\n", m);
+        printf("%d-%d\n", m, c4);
 
-    m = amount_of_money / 1000;
-    amount_of_money -= m * 1000;
+    m = amount_of_money / c5;
+    amount_of_money -= m * c5;
     if (m > 0)
-        printf("%d-1000\n", m);
+        printf("%d-%d\n", m, c5);
 
-    m = amount_of_money / 500;
-    amount_of_money -= m * 500;
+    m = amount_of_money / c6;
+    amount_of_money -= m * c6;
     if (m > 0)
-        printf("%d-500\n", m);
+        printf("%d-%d\n", m, c6);
 
-    m = amount_of_money / 200;
-    amount_of_money -= m * 200;
+    m = amount_of_money / c7;
+    amount_of_money -= m * c7;
     if (m > 0)
-        printf("%d-200\n", m);
+        printf("%d-%d\n", m, c7);
 
-    m = amount_of_money / 100;
-    amount_of_money -= m * 100;
+    m = amount_of_money / c8;
+    amount_of_money -= m * c8;
     if (m > 0)
-        printf("%d-100\n", m);
+        printf("%d-%d\n", m, c8);
 
-    m = amount_of_money / 50;
-    amount_of_money -= m * 50;
+    m = amount_of_money / c9;
+    amount_of_money -= m * c9;
     if (m > 0)
-        printf("%d-50\n", m);
+        printf("%d-%d\n", m, c9);
 
-    m = amount_of_money / 20;
-    amount_of_money -= m * 20;
+    m = amount_of_money / c10;
+    amount_of_money -= m * c10;
     if (m > 0)
-        printf("%d-20\n", m);
+        printf("%d-%d\n", m, c10);
 
-    m = amount_of_money / 10;
-    amount_of_money -= m * 10;
+    m = amount_of_money / c11;
+    amount_of_money -= m * c11;
     if (m > 0)
-        printf("%d-10\n", m);
+        printf("%d-%d\n", m, c11);
 
-    m = amount_of_money / 5;
-    amount_of_money -= m * 5;
+    m = amount_of_money / c12;
+    amount_of_money -= m * c12;
     switch (amount_of_money) {
     case (3):
     case (4):
         m++;
     }
     if (m > 0)
-        printf("%d-5\n", m);
+        printf("%d-%d\n", m, c12);
 
     return 0;
 }
