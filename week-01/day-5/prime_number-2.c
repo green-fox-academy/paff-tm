beead55087712291d53374830e112fc18bad6d3b
paff-tm <4ndras@gmail.com>
2017-10-06 10:44:35 +0200

    upload an other solution of task of prime numbers

---
+++ week-01/day-5/prime_number-2.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/*
+Create a program which prints out oll the prime numbers
+aslo add a feature where you can tell the from - to limits
+for example :
+from = 11;
+to = 34;
+output :
+13, 17, 19, 23, 29, 31 , this is 6 prime numbers
+*/
+
+void main()
+{
+    int from = -3;
+    int to = 10;
+    int divider_count = 0;
+    int prime_count = 0;
+    int j = 0;
+
+    for (int i = from; i <= to; i++) {
+        if (i > 1) {
+            j = 2;
+            divider_count = 1;
+            do {
+                if (i % j == 0){
+                    divider_count++;
+                }
+                j++;
+            } while ((divider_count < 3) && (j <= i));
+            if (divider_count == 2){
+                printf("%d, ", i);
+                prime_count++;
+            }
+        }
+    }
+    printf("this is %d prime numbers", prime_count);
+
+    return 0;
+}
