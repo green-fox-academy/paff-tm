96d627466a81270bd62016484248d6e243a69452
paff-tm <4ndras@gmail.com>
2017-10-06 13:08:36 +0200

    corrects the power calculation, will work fine

--- week-01/day-5/armstrong.c
+++ week-01/day-5/armstrong.c
@@ -19,24 +19,25 @@ void main()
         aa /= 10;
         digits++;
     } while (aa > 0);
-    printf("%d is the digits of the given number\n", digits);
+    //printf("%d is the digits of the given number\n", digits);
 
 // check if the given number is an armstrong number
     aa = a;
     for (int i = 1; i <= digits; i++) {
-        aa_pwr = aa % 10;                   // aa_pwr is the last digit of the given number
-        printf("induló digit: %d\n",aa_pwr);
-        for (int j = 0; j < digits; j++)    // aa_pwr will be powered aa
-            aa_pwr *= aa_pwr;
-        printf("hatványos aa: %d\n",aa_pwr);
+        aa_pwr = aa % 10;                   // aa_pwr will be the last digit of the given number
+        //printf("%d\tis the digit to calculate with\n",aa_pwr);
+        for (int j = 1; j < digits; j++)    // aa_pwr will be powered with the last digit of a
+            aa_pwr *= aa % 10;
+        //printf("%d\tis the powered digit\n",aa_pwr);
         arm_sum += aa_pwr ;                 // add the power of the last digit to sum
         aa /= 10;                           // cut the last digit
     }
-    printf("%d is the sum of the powers of the digits\n", arm_sum);
+    //printf("%d\tis the sum of the powers of the digits\n", arm_sum);
 
+// print the result
     if (arm_sum == a)
-        printf("The %d is an Armstrong number\n", a);
+        printf("\nThe %d is an Armstrong number\n", a);
     else
-        printf("The %d is not an Armstrong number\n", a);
+        printf("\nThe %d is not an Armstrong number\n", a);
     return 0;
 }
