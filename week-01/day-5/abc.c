ac171bbb69a9202effe0dd13b10d72d213585d1e
paff-tm <4ndras@gmail.com>
2017-10-06 13:16:19 +0200

    add an other solution to abc.c

--- week-01/day-5/abc.c
+++ week-01/day-5/abc.c
@@ -8,5 +8,10 @@ int main()
     for (int i = 65; i < 91; i++) {
         printf("%c%c", i + 32, i);
     }
+// an other - maybe better -  solution
+    printf("\n");
+    for (char i = 'A'; i <= 'Z'; i++) {
+        printf("%c%c", i + ('a' - 'A'), i);
+    }
 	return 0;
 }
