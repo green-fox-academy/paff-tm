5da59a849cb7f6689cda94216a17050631602c37
paff-tm <4ndras@gmail.com>
2017-10-10 15:36:53 +0200

    uplead the empty file for task advanced_3

---
+++ week-02/day-2/advanced_3/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int arr[20];
+    int freq[20];
+
+    // TODO: write a program wich asks for a number, that will be the real size of the array (20 elements are allocated, that's the maximum size)
+    // Then load up the array with integer values
+    // Finally print all unique elements in array
+
+
+    return 0;
+}
