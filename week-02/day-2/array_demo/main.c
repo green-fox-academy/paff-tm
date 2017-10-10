47519f5f29bd2d0f3e36e2227f4e4c8f765fc20e
paff-tm <4ndras@gmail.com>
2017-10-10 10:20:49 +0200

    upload array demo

---
+++ week-02/day-2/array_demo/main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char array[] = "cica";
+    //char array2[] = {'c', 'i', 'c', 'a', '\0'};
+
+    //printf("%c", array[1]);
+    printf("%s\n", array);
+    puts(array);
+
+    scanf("%s", array);
+    puts(array);
+
+    printf("%d", sizeof(array));
+
+    for (int i = 0; i < sizeof(array); i++)
+        printf("%c", array[i]);
+
+    printf("mas\n");
+
+    int array3[2][3] = {
+        (1, 2, 3),
+        (3, 4, 5),
+    };
+
+    for (int i = 0; i < 2; i++){
+        for (int j = 0; j < 3; j++) {
+            printf("%d", array3[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
