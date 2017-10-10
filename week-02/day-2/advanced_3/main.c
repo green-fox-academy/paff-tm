4eedae6798333c1bf4036f2fcac4f831601e3b7c
paff-tm <4ndras@gmail.com>
2017-10-10 15:52:20 +0200

    uplead the first try of task advanced_3 - prints out all elements not only uniqe ones

--- week-02/day-2/advanced_3/main.c
+++ week-02/day-2/advanced_3/main.c
@@ -5,10 +5,29 @@ int main()
     int arr[20];
     int freq[20];
 
-    // TODO: write a program wich asks for a number, that will be the real size of the array (20 elements are allocated, that's the maximum size)
+    // TODO: write a program wich asks for a number, that will be the real size of the
+    // array (20 elements are allocated, that's the maximum size)
     // Then load up the array with integer values
     // Finally print all unique elements in array
 
+    int real_size_of_array = 0;
+    do {
+        printf("Add the size of the array (1-20): ");
+        scanf("%d", &real_size_of_array);
+        if ((real_size_of_array > 0) && (real_size_of_array < sizeof(arr) / sizeof(arr[0]))){
+            break;
+        }
+    } while (1);
+
+ // fill up integer values
+    for (int i = 0; i < real_size_of_array; i++) {
+        arr[i] = rand()%1000;
+    }
+
+ // print all item of array
+     for (int i = 0; i < real_size_of_array; i++) {
+        printf("%d\n", arr[i]);
+    }
 
     return 0;
 }
