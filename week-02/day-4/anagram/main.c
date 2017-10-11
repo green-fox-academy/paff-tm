d62db899fb789a565d6800aa433b80102c677a01
paff-tm <4ndras@gmail.com>
2017-10-11 15:39:29 +0200

    upload the working version of anagram

--- week-02/day-4/anagram/main.c
+++ week-02/day-4/anagram/main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int letter_count(char input1[256], char input2[256]);
+
 int main()
 {
     char input1[256];
     char input2[256];
     int is_anagram = 1;
-    int letter_is = 0;
+    int letter_count1 = 0;
+    int letter_count2 = 0;
 
     printf("Check if the two given words are anagram.\n");
     printf("Add the first word: ");
@@ -15,19 +18,28 @@ int main()
     gets(input2);
 
     if (strlen(input1) == strlen(input2)) {
-        for (int i = 0; i < strlen(input1); i++){
-            letter_is = 0;
-            for (int j = 0; j < strlen(input2); j++) {
-                if (input1[i] == input2[j]) {
-                    letter_is = 1;
-                    break;
+        for (int i = 0; i < strlen(input1); i++) {
+            // counts the input1 ith letters in input1
+            letter_count1 = 0;
+            for (int j = 0; j < strlen(input1); j++) {
+                if (input1[j] == input1[i]) {
+                    letter_count1++;
                 }
             }
-            if (letter_is == 0) {
+            // counts the input1 ith letters in input2
+            letter_count2 = 0;
+            for (int j = 0; j < strlen(input2); j++) {
+                if (input2[j] == input1[i]) {
+                    letter_count2++;
+                }
+            }
+            if (letter_count1 != letter_count2) {
                 is_anagram = 0;
                 break;
             }
         }
+    } else {
+        is_anagram = 0;
     }
 
     if (is_anagram)
@@ -36,4 +48,9 @@ int main()
         printf("These are not anagrams!\n");
 
     return 0;
+}
+
+int letter_count(char input1[256], char input2[256])
+{
+
 }
