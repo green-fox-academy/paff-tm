cd4eaaa4bf72b84eb384caad5e98a0ba1cfe7153
paff-tm <4ndras@gmail.com>
2017-10-11 14:38:57 +0200

    update task gcd with .h file, and remove test folder

--- week-02/day-1/gcd/main.c
+++ week-02/day-1/gcd/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "gcd.h"
 
 // create a function to find the greatest common divisor of two numbers (a and b)
 // again the parameters value should be stored in a .h file
@@ -9,9 +10,6 @@ int gcd(int a, int b);
 
 int main()
 {
-    int a = 20;
-    int b = 12;
-
     printf("The greatest common divisor of %d and %d is %d", a, b, gcd(a,b));
 
     return 0;
