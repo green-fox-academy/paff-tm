45f9730482eb5110e334c1b606760605daea10d2
paff-tm <4ndras@gmail.com>
2017-10-12 13:46:02 +0200

    upload a step-by-step solution for josephus problem

--- week-02/day-4/Josephus_problem/main.c
+++ week-02/day-4/Josephus_problem/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 int josephus(int people);
+int next_alive(int group[], int people, int starting_seat);
 
 int main()
 {
@@ -16,11 +17,47 @@ int main()
     return 0;
 }
 
+// step-by-step solution, gives back the nr. of the winning seat from 1
 int josephus(int people) {
-    int winning_seat = 0;
+    int group[people];  //1 is alive, 0 is dead
+    int seat = 0;
+    int n = 0;
+    int step = 0;
 
+    for (int i = 0; i < people; i++)
+        group[i] = 1;
 
-    return winning_seat;
+    do {
+        step++;
+        seat = n;
+        n = next_alive(group, people, seat);
+        if (n != seat) {
+            group[n] = 0;
+            n = next_alive(group, people, seat);
+            //n = next_alive(group, people, seat);
+        }
+        printf("After the %d. step:\t", step);
+        for (int i = 0; i < people; i++)
+            printf("%d", group[i]);
+        printf("\n");
+    } while (n != seat);
+
+    return seat + 1;
+}
+
+// find the next alive person's seat in the array (for josephus() )
+int next_alive(int group[], int people, int starting_seat)
+{
+    int i = starting_seat;
+    do {
+        if (i == people - 1) {
+            i = 0;
+        } else {
+            i++;
+        }
+    } while (group[i] == 0 && i != starting_seat);
+
+    return i;
 }
 
 
