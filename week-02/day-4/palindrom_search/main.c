d336f6571c3c9b35bfc8fd1b0dfacbfcaab78bcb
paff-tm <4ndras@gmail.com>
2017-10-12 14:22:03 +0200

    update palindrome search with comments and replace variable names to be more understandable

--- week-02/day-4/palindrom_search/main.c
+++ week-02/day-4/palindrom_search/main.c
@@ -22,43 +22,56 @@ int main()
     return 0;
 }
 
+/*  Search the palindroms in the input string, and gives back them in output string
+ *  Return 1 if the input is too short (less than 3 characters), else return 0;
+ */
 int search_palind(char input[], char output[])
 {
-    char temp[256] = "";
-    int l = 0; //l as length of the palidrome counted from central of the palindrome
-    int t = 0;
+    char temp[256] = "";// temp string for palindrome
+    int pal_len = 0;    // length of the palidrome counted from central of the palindrome
+    int pal_cen = 0;    // central position of palindrome in the input string
+    char temp[256] = "";// temp string for palindrome
+    int t = 0;          // temp for 'temp' string
 
+    // initialize the beginning of output
     output[0] = '[';
     output[1] = '\0';
 
     if (strlen(input) < 3) {
         return 1;
     }
-    for (int c = 1; c < strlen(input) - 1; c++) { //c as central position of palindrome
-        l = 0;
-        while ((l <= c) && (l < strlen(input) - c) && (input[c - l] == input[c + l])) {
-            l++;
+    for (pal_cen = 1; pal_cen < strlen(input) - 1; pal_cen++) {
+        // check the length of the palindrome at the pal_cen position
+        pal_len = 0;
+        while ((pal_len <= pal_cen) && (pal_len < strlen(input) - pal_cen) && (input[pal_cen - pal_len] == input[pal_cen + pal_len])) {
+            pal_len++;
         }
-        l--;
-        while (l > 0) {
-            //copy the polidrome to the output sting
+        pal_len--;
+
+        //copy the palidrome to the output sting
+        while (pal_len > 0) {
+            // add the opening " before the palindrome, and a ',' if it is not the first
             if (strlen(output) > 1)
-                strcat(output, ", \"");
-            else
-                strcat(output, "\"");
+                strcat(output, ", ");
+            strcat(output, "\"");
+
+            // copy the palindrome to temp string
             t = 0;
-            for (int i = c - l; i <= c + l; i++) {
+            for (int i = pal_cen - pal_len; i <= pal_cen + pal_len; i++) {
                 temp[t] = input[i];
                 t++;
             }
             temp[t] = '\0';
-            strcat(output, temp);
-            strcat(output, "\"");
 
-            l--;
+            //copy the temp string to output string
+            strcat(output, temp);
+            // add the closing " to output
+            strcat(output, "\"");
+            // at a longer palidrome, there are more, (shorter) palindroms in it, so at the next round add the shorter one to output
+            pal_len--;
         }
     }
-
+    // add the closing ']' to output
     strcat(output, "]");
 
     return 0;
