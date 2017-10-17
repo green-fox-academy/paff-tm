b6544347df6638acd8b345d89d6e785e590ead7c
paff-tm <4ndras@gmail.com>
2017-10-17 09:51:39 +0200

    upload task 02

---
+++ week-03/day-2/02/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+	double x = 1.2;
+
+    //TODO: write a program wich adds up x and 1, and print out it rounded.
+
+    printf("%d", (int)x + 1);
+
+    return 0;
+}
