8b1cb3bd56e2bd6b2a16bd4d47fab6d62ae6a8d3
paff-tm <4ndras@gmail.com>
2017-10-17 13:34:15 +0200

    update task 08 with error handling

--- week-03/day-2/08/main.c
+++ week-03/day-2/08/main.c
@@ -10,16 +10,33 @@ void transform (char str[], int original_base, int new_base);
 
 int main()
 {
-    char str[20];
+    char str[32]; //= "-2147483649";
     int original_base;
     int new_base;
 
+
     printf("Give me a number to transform it in an other system: ");
     scanf("%s", str);
-    printf("Give me the original base: ");
-    scanf("%d", &original_base);
+
+    // get the original base of the given number
+    while (1) {
+        printf("Give me the original base: ");
+        scanf("%d", &original_base);
+        if (original_base < 2 || original_base > 36)
+            printf("The value should be between 2 and 36. Give it again: ");
+        else
+            break;
+    }
+
+    // get the new base as a number between 2 and 36
     printf("Give me the number of the new base: ");
-    scanf("%d", &new_base);
+    while (1) {
+        scanf("%d", &new_base);
+        if (new_base < 2 || new_base > 36)
+            printf("The value should be between 2 and 36. Give it again: ");
+        else
+            break;
+    }
 
     transform(str, original_base, new_base);
 
@@ -28,12 +45,16 @@ int main()
 
 void transform (char str[], int original_base, int new_base)
 {
-    char str_new_number[20] = "";
+    char str_new_number[32] = "";
     long int value = 0;
+    char *ptr = 0;
 
-    value = strtol(str, 0, original_base);
-    printf("number in base %d: %d\n", original_base, value);
-
-    ltoa(value, str_new_number, new_base);
-    printf("number in base %d: %s\n", new_base, str_new_number);
+    value = strtol(str, &ptr, original_base);
+    if (ptr != str) {
+        printf("number in base 10: %d\n", value);
+        ltoa(value, str_new_number, new_base);
+        printf("number in base %d: %s\n", new_base, str_new_number);
+    } else {
+        printf("Error: The given string can't be converted to integer.\n");
+    }
 }
