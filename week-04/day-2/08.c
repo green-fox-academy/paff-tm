0760406d0e36a16d2a1194ca401e7e6ecd7fa59b
paff-tm <4ndras@gmail.com>
2017-10-25 16:57:49 +0200

    upload task 08

---
+++ week-04/day-2/08.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+typedef struct complex
+{
+    float real;
+    float imag;
+} complex;
+
+
+//TODO: create 3 functions, which calculates the sum, subtraction and multiplication of a complex number.
+// you can read about complex number's operations here: https://www.mathsisfun.com/numbers/complex-numbers.html
+
+complex comp_add(complex a, complex b);
+complex comp_sub(complex a, complex b);
+complex comp_mul(complex a, complex b);
+
+int main()
+{
+    complex n1, n2;
+    n1.real = 5;
+    n1.imag = 7.2;
+
+    n2.real = 6;
+    n2.imag = 8.1;
+
+    printf("nr1: (%g + %gi); nr2 (%g + %gi)\n", n1.real, n1.imag, n2.real, n2.real);
+
+    printf("result of addition:       (%g + %gi)\n", comp_add(n1, n2).real, comp_add(n1, n2).imag);
+    printf("result of subtraction:    (%g + %gi)\n", comp_sub(n1, n2).real, comp_sub(n1, n2).imag);
+    printf("result of multiplication: (%g + %gi)\n", comp_mul(n1, n2).real, comp_mul(n1, n2).imag);
+
+    return 0;
+}
+
+complex comp_add(complex a, complex b)
+{
+    complex result;
+
+    result.real = a.real + b.real;
+    result.imag = a.imag + b.real;
+
+    return result;
+}
+
+complex comp_sub(complex a, complex b)
+{
+    complex result;
+
+    result.real = a.real - b.real;
+    result.imag = a.imag - b.real;
+
+    return result;
+}
+
+complex comp_mul(complex a, complex b)
+{
+    complex result;
+
+    result.real = a.real * b.real - a.imag * b.real;
+    result.imag = a.real * b.imag + a.imag * b.real;
+
+    return result;
+}
