c7584aca5a9207966cddac76a46f9f3868ff05fb
paff-tm <4ndras@gmail.com>
2017-11-13 14:45:12 +0100

    update vector: 01.cpp to add values for the vector

--- week-06/day-1/vector/01.cpp
+++ week-06/day-1/vector/01.cpp
@@ -9,6 +9,10 @@ int main()
 	vector <int> int_vector;
 	int_vector.reserve(5);
 
+	for (int i = 0; i < int_vector.capacity(); ++i) {
+        int_vector.push_back(i);
+	}
+
 	for (int i = 0; i < int_vector.size(); ++i) {
         cout << int_vector.at(i) << endl;
 	}
