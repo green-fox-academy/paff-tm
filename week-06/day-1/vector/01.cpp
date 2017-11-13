8dd555f1092302dece926f8442e483a27b0242cb
paff-tm <4ndras@gmail.com>
2017-11-13 14:43:56 +0100

    upload vector: 01.cpp

---
+++ week-06/day-1/vector/01.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+	//create an integer vector with the size of 5 and print all elements of it
+	vector <int> int_vector;
+	int_vector.reserve(5);
+
+	for (int i = 0; i < int_vector.size(); ++i) {
+        cout << int_vector.at(i) << endl;
+	}
+
+    return 0;
+}
