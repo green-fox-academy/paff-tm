06f72792c12cb90d894468cf5af2a30fce4db4b2
paff-tm <4ndras@gmail.com>
2017-11-13 11:31:57 +0100

    init the new week, upload 00.cpp and init 01.cpp

---
+++ week-06/day-1/00.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+// Modify this program to greet you instead of the World!
+
+int main()
+{
+    cout << "Hello Me!" << endl;
+    return 0;
+}
---
+++ week-06/day-1/01.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+// Store their names in separate string type variables
+
+
+int main()
+{
+
+    return 0;
+}
