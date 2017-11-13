2d43b2d386fc1af94e879da4ec2a040ff9d77606
paff-tm <4ndras@gmail.com>
2017-11-13 13:45:56 +0100

    upload DMA: 01.cpp

---
+++ week-06/day-1/DMA/01.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+// Please allocate a 5 long array and fill it with numbers from 0 to 4, then print the whole array
+// Please delete the array before the program exits
+
+int main() {
+    int* pointer = NULL;
+    pointer = new int[5];
+
+    for (int i = 0; i < 5; ++i) {
+        pointer[i] = i;
+    }
+
+    for (int i = 0; i < 5; ++i) {
+        cout << pointer[i] << endl;
+    }
+
+    delete pointer;
+
+    return 0;
+}
