9d11a242c84909b19049311aebbc44f36425b6b4
paff-tm <4ndras@gmail.com>
2017-11-13 12:42:32 +0100

    init variables in strings: 02.cpp

---
+++ week-06/day-1/strings/02.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+// Define several things as a variable then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a double
+// Whether you are married or not as a boolean
+
+int main()
+{
+    string name = "Andris";
+    int age = 33;
+    double height = 1.8;
+    bool married = true;
+    
+    
+
+    return 0;
+}
