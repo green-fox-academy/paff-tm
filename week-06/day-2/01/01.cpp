ef1a12a4ef188c07da37a5a07c935a8fb83a4210
paff-tm <4ndras@gmail.com>
2017-11-14 11:00:25 +0100

    update task 01 according to styleguide

--- week-06/day-2/01/01.cpp
+++ week-06/day-2/01/01.cpp
@@ -9,15 +9,16 @@ class Student {
     unsigned int age;
 };
 
-int main() {
-  // Create a new instace of the Student class and set it's name to "John" and
-  // it's age to 21
-  Student student;
+int main() 
+{
+    // Create a new instace of the Student class and set it's name to "John" and
+    // it's age to 21
+    Student student;
 
-  student.name = "John";
-  student.age = 21;
+    student.name = "John";
+    student.age = 21;
 
-  cout << student.name << " " << student.age << endl;
+    cout << student.name << " " << student.age << endl;
 
-  return 0;
+    return 0;
 }
