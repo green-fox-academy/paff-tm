9cb419a720c854332154ec253092b8cc7852049a
paff-tm <4ndras@gmail.com>
2017-11-14 11:49:59 +0100

    update task 06 - methods and main

--- week-06/day-2/06/06.cpp
+++ week-06/day-2/06/06.cpp
@@ -7,17 +7,25 @@ class Car {
 private:
     string type;
     unsigned int km;
-    
+
 public:
     Car(string type, unsigned int km) {
         this->type = type;
         this->km = km;
     }
-    
+
     ~Car() {
-    
+        cout << "The car is demolished" << endl;;
     }
-    
+
+    void run(unsigned int run_km) {
+        km += run_km;
+    }
+
+    void print_data(){
+        cout << type << " " << km << endl;
+    }
+
 };
 
 int main()
@@ -27,6 +35,12 @@ int main()
     // It should have a "km" property that stores how many kilometers it have run
     // The km and the type property should be a parameter in the constructor
     // It should have a method called "run" that takes a number and increments the "km" property by it
+
+    Car c("Ford", 137032);
+
+    c.print_data();
+    c.run(8);
+    c.print_data();
 
     return 0;
 }
