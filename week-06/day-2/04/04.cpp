b6607ee1fd8c2a9f4d2fdfb3704520dc17b16e5a
paff-tm <4ndras@gmail.com>
2017-11-14 11:10:14 +0100

    update task 04 get_price to print full price

--- week-06/day-2/04/04.cpp
+++ week-06/day-2/04/04.cpp
@@ -21,17 +21,17 @@ class House {
     }
 
     unsigned int get_price() {
-      return this->price;
+      return this->price * this->area;
     }
 
 };
 
 int main() {
-    
+
   // The market price of the houses is 400 EUR / square meters
   // Make the get_price() function work
   // Create a constructor for the House class that takes it's address and area.
-  
+
   House house = House("Andrassy 66", 349);
   cout << house.get_price();
 
