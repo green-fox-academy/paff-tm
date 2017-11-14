f9ff8fff961290291a2264109603e6886c6026c8
paff-tm <4ndras@gmail.com>
2017-11-14 13:07:51 +0100

    update task 08 - methods and main

--- week-06/day-2/08/08.cpp
+++ week-06/day-2/08/08.cpp
@@ -1,18 +1,39 @@
 #include <iostream>
-#include <string>
+#include <vector>
+#include <numeric>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
 class Student {
 private:
+    vector <int> *grades;
 
 public:
     Student() {
-
+        grades = new vector <int>;
     }
 
     ~Student() {
         cout << "The student has died..." << endl;
+        delete grades;
+    }
+
+    void add_grade(unsigned int grade) {
+        if (grade > 0 && grade < 6) {
+            grades->push_back(grade);
+        }
+    }
+
+    float get_average() {
+        int sum;
+        if (grades->size() == 0) {
+            return 0;
+        } else {
+            sum = accumulate(grades->begin(), grades->begin() + grades->size(), 0);
+        }
+        return (float)sum / grades->size();
     }
 
 };
@@ -24,7 +45,15 @@ int main()
     // an other method `get_average`, that returns the average of the
     // grades
 
+    srand(time(0));
+
     Student pupil;
+
+    for (int i = 0; i < 10; ++i) {
+        pupil.add_grade(rand() % 5 + 1);
+    }
+
+    cout << pupil.get_average() << endl;
 
     return 0;
 }
