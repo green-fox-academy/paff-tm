1d878b1a7d1a49e8620c5462777e1dfd8e4d3caf
paff-tm <4ndras@gmail.com>
2017-11-15 11:33:15 +0100

    task 00: correct travelling() functions in all classes and make main

--- week-06/day-3/00/main.cpp
+++ week-06/day-3/00/main.cpp
@@ -9,18 +9,21 @@ using namespace std;
 // For example: Human is walking. SuperHuman is flying. AquaHuman is riding with dolphins.
 
 class Human {
+    public:
     virtual void travelling(){
         cout << "I'm walkin' in the rain..." << endl;
     };
 };
 
-class SuperHuman: private Human {
+class SuperHuman: public Human {
+    public:
     virtual void travelling(){
         cout << "I believe I can fly...!" << endl;
     };
 };
 
-class AquaHuman: private Human {
+class AquaHuman: public Human {
+    public:
     virtual void travelling(){
         cout << "I can swim!" << endl;
     };
@@ -29,7 +32,13 @@ class AquaHuman: private Human {
 
 int main()
 {
+    Human h;
+    SuperHuman sh;
+    AquaHuman ah;
 
+    h.travelling();
+    sh.travelling();
+    ah.travelling();
 
     return 0;
 }
