7703f63d07bab66ab88235175b3cfef2f892ac29
paff-tm <4ndras@gmail.com>
2017-11-15 13:14:26 +0100

    update task 01, done

--- week-06/day-3/01/main.cpp
+++ week-06/day-3/01/main.cpp
@@ -10,20 +10,51 @@ using namespace std;
 // the class (so it is a private method)
 
 class Shape {
-
+    public:
+        float getArea() {
+            return area;
+        };
+    protected:
+        float area;
+        virtual void calc_area() = 0;
 };
 
 class Circle: public Shape {
-
+    public:
+        Circle(float r) {
+            this->r = r;
+            calc_area();
+        };
+    private:
+        float r;
+        virtual void calc_area() {
+            area =  r *r * 3.1415;
+        };
 };
 
 class Triangle: public Shape {
-
+    public:
+        Triangle(float a, float m) {
+            this->a = a;
+            this->m = m;
+            calc_area();
+        };
+    private:
+        float a;
+        float m;
+        virtual void calc_area() {
+            area = (a * m) / 2;
+        };
 };
 
 
 int main()
 {
+    Circle c(1);
+    Triangle t(2, 1);
+
+    cout << c.getArea() << endl;
+    cout << t.getArea() << endl;
 
     return 0;
 }
