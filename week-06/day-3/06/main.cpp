8d27e6e80b1603009bf254cfef3eec709fbe9dc2
paff-tm <4ndras@gmail.com>
2017-11-15 14:55:27 +0100

    init task 06

---
+++ week-06/day-3/06/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Create a linked list using classes.
+// If you are not similar whit linked lists look it up on the internet.
+// Feel free to walk through some tutorials, get familiar with your implementation.
+
+int main()
+{
+    return 0;
+}
