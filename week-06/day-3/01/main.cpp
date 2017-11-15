622e87b6685090181c80425674abe6741f4c738a
paff-tm <4ndras@gmail.com>
2017-11-15 12:54:52 +0100

    init task 01, create classes

---
+++ week-06/day-3/01/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Create a Shape class, inherit two derived class from it
+// One should be Circle, and the other one is Triangle
+// For each derived class, implement a calc_area() method
+// which calculates the area of the shape and stores it inside
+// the class (so it is a private method)
+
+class Shape {
+
+};
+
+class Circle: public Shape {
+
+};
+
+class Triangle: public Shape {
+
+};
+
+
+int main()
+{
+
+    return 0;
+}
