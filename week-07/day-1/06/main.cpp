9ca3abdf33bbb44b8fdcc81f163deef58d275c6c
paff-tm <4ndras@gmail.com>
2017-11-20 13:22:11 +0100

    upload task 06 and correct task 05

--- week-07/day-1/06/main.cpp
+++ week-07/day-1/06/main.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
+#include <stdexcept>
 using namespace std;
 
-// Create a function which throws an char
+// Create a function which throws a bulit-in exception
 // In the main(), put that function in a try block
 // Also in the main() catch, what your function throws
 
 void check0division(int x)
 {
     if (x == 0)
-        throw '0';
+        throw runtime_error("division by 0");
 }
 
 int main()
@@ -21,8 +22,8 @@ int main()
         check0division(b);
         cout << a / b;
     }
-    catch (char x) {
-        cout << "Error: " << x << endl;
+    catch (runtime_error &err) {
+        cout << "Error: " << err.what() << endl;
     }
 
 	return 0;
