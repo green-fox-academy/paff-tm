94aa8943b9845159f0cf12a8afc93c83d0ef924e
paff-tm <4ndras@gmail.com>
2017-11-20 12:51:20 +0100

    upload task 02

--- week-07/day-1/02/main.cpp
+++ week-07/day-1/02/main.cpp
@@ -1,9 +1,18 @@
 #include <iostream>
-
 using namespace std;
+
+// Write a try - catch block.
+// Throw an char in the try block
+// Catch it in the catch block and write it out.
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+    try {
+        throw 'x';
+    }
+    catch (char c) {
+        cout << "Error: " << c << endl;
+    }
+
+	return 0;
 }
