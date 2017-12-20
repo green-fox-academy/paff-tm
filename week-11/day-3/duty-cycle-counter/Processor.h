38815fdd94693087d3a778a3236f85fb9395765b
paff-tm <4ndras@gmail.com>
2017-12-20 21:50:29 +0100

    Init class Processor

---
+++ week-11/day-3/duty-cycle-counter/Processor.h
@@ -0,0 +1,17 @@
+#ifndef PROCESSOR_H
+#define PROCESSOR_H
+
+#include "PWM.h"
+
+class Processor
+{
+    public:
+        Processor();
+        ~Processor();
+
+    protected:
+
+    private:
+};
+
+#endif // PROCESSOR_H
