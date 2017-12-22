60596184aa776837eab43ed4b3b08e7cec85fa49
paff-tm <4ndras@gmail.com>
2017-12-22 16:00:38 +0100

    Upload bike_shift_indicator project init. LEDs can light with software PWM.

---
+++ STM32Cube_FW_F7_V1.8.0/Projects/STM32746G-Discovery/GreenFox/bike_shift_indicator/Inc/define.h
@@ -0,0 +1,28 @@
+/*
+ * 	Define file for ports
+ */
+
+#ifndef DEFINE_H
+#define DEFINE MAIN_H
+
+#define D3_PIN			GPIO_PIN_4
+#define D3_PORT			GPIOB
+#define D5_PIN			GPIO_PIN_0
+#define D5_PORT			GPIOI
+#define D7_PIN			GPIO_PIN_3
+#define D7_PORT			GPIOI
+#define D10_PIN			GPIO_PIN_8
+#define D10_PORT		GPIOA
+#define D14_PIN			GPIO_PIN_9
+#define D14_PORT		GPIOB
+#define D15_PIN			GPIO_PIN_8
+#define D15_PORT		GPIOB
+
+#define	D3				D3_PORT, D3_PIN
+#define	D5				D5_PORT, D5_PIN
+#define	D7				D7_PORT, D7_PIN
+#define	D10				D10_PORT, D10_PIN
+#define	D14				D14_PORT, D14_PIN
+#define	D15				D15_PORT, D15_PIN
+
+#endif
