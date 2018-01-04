6e07e38377787564af33da8a9318d8faa1c82200
paff-tm <4ndras@gmail.com>
2018-01-04 22:14:54 +0100

    Create new project of URM

---
+++ STM32Cube_FW_F7_V1.8.0/Projects/STM32746G-Discovery/GreenFox/Ultrasonic_Ranging_Module/Inc/define.h
@@ -0,0 +1,31 @@
+/*
+ * 	Define file for ports
+ */
+
+#ifndef DEFINE_H
+#define DEFINE MAIN_H
+
+#define D3_PIN			GPIO_PIN_4
+#define D3_PORT			GPIOB
+#define D5_PIN			GPIO_PIN_0
+#define D5_PORT			GPIOI
+#define D7_PIN			GPIO_PIN_3
+#define D7_PORT			GPIOI
+
+#define D9_PIN			GPIO_PIN_15
+#define D9_PORT			GPIOA
+#define D10_PIN			GPIO_PIN_8
+#define D10_PORT		GPIOA
+#define D14_PIN			GPIO_PIN_9
+#define D14_PORT		GPIOB
+#define D15_PIN			GPIO_PIN_8
+#define D15_PORT		GPIOB
+
+#define	D3				D3_PORT, D3_PIN
+#define	D5				D5_PORT, D5_PIN
+#define	D9				D9_PORT, D9_PIN
+#define	D10				D10_PORT, D10_PIN
+#define	D14				D14_PORT, D14_PIN
+#define	D15				D15_PORT, D15_PIN
+
+#endif
